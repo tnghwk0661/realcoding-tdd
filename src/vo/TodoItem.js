9b7fdea76ec599b1ba082d1a91c391d23e3fdb47
tnghwk0661 <tnghwk0661@gmail.com>
2022-05-11 19:07:55 +0900

    up-to-date

--- src/vo/TodoItem.js
+++ src/vo/TodoItem.js
@@ -1,4 +1,4 @@
-import { makeObservable, observable } from "mobx";
+import { makeObservable, observable, action, computed } from "mobx";
 
 class TodoItem {
   /*
@@ -12,12 +12,34 @@ class TodoItem {
   _createdAt = "";
   _completed = false;
 
-  constructor() {
+  constructor(id, task, createdAt, completed = false) {
     makeObservable(this, {
       _task: observable,
       _completed: observable,
     });
+    this._id = id;
+    this._task = task;
+    this._createdAt = createdAt;
+    this._completed = completed;
   }
+
+  updateTask = (task) => {
+    this._task = task;
+  };
+
+  setComplete = () => {
+    this._completed = true;
+  };
+
+  unsetComplete = () => {
+    this._completed = false;
+  };
+
+  equalsDayOfCreatedAt = (date) => {
+    const sourceDate = new Date(this._createdAt).setHours(0, 0, 0, 0);
+    const targetDate = new Date(date).setHours(0, 0, 0, 0);
+    return sourceDate === targetDate;
+  };
 
   get id() {
     return this._id;
--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -1,4 +1,4 @@
-import { makeObservable, observable } from "mobx";
+import { makeObservable, observable, action, computed } from "mobx";
 
 class TodoList {
   /*
@@ -8,11 +8,56 @@ class TodoList {
   _items = [];
   _date = "";
 
-  constructor() {
+  constructor(items, date) {
     makeObservable(this, {
       _items: observable,
     });
+    this._items = items || [];
+    this._date = date;
   }
+
+  _completedFilter = (todoItem) => todoItem.completed;
+  _notCompletedFilter = (todoItem) => !todoItem.completed;
+
+  _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreatedAt(this._date);
+  _notEqualsDayFilter = (todoItem) =>
+    !todoItem.equalsDayOfCreatedAt(this._date);
+
+  get equalsDayItems() {
+    return this._items.filter(this._equalsDayFilter);
+  }
+
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter(this._completedFilter);
+  }
+
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter(this._notCompletedFilter);
+  }
+
+  get notEqualsDayItems() {
+    return this._items.filter(this._notEqualsDayFilter);
+  }
+
+  get notEqualsDayAndCompletedItems() {
+    return this.notEqualsDayItems.filter(this._completedFilter);
+  }
+
+  get notEqualsDayAndNotCompletedItems() {
+    return this.notEqualsDayItems.filter(this._notCompletedFilter);
+  }
+
+  pushTodoItem = (todoItem) => {
+    this._items.push(todoItem);
+  };
+
+  removeTodoItem = (todoId) => {
+    const targetTodoItemIndex = this._items.findIndex(
+      (todo) => todo.id === todoId
+    );
+    if (targetTodoItemIndex === -1) return;
+    this._items.splice(targetTodoItemIndex, 1);
+  };
 }
 
 export default TodoList;
